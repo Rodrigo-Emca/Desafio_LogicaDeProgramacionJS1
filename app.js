17ace6a3256ece033018590266696597759c55a2
Rodrigo Cabrera <rodrigocabrera.developer@gmail.com>
2025-01-28 16:19:35 -0300

    Desafio 4: funcion de promt y alert, usando variable

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ titulo.innerHTML = "Hora del Desafio";
 
 //3. Crea una función que muestre en la consola el mensaje "El botón fue clickeado" siempre que se presione el botón "Console".
 
-function alertaDeClick() {
+function alertaDeClickConsola() {
     console.log("El botón fue clickeado")
+}
+
+//4. Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
+
+function alertaPromt() {
+    let ciudad = prompt('Escriba el nombre de alguna ciudad de Brasil');
+    //Para usar la concatenación en js, entrecomillamos parte de la frase, luego + y la variable.
+    alert("Estuve en " + ciudad + " y me acordé de ti.");
+    /*Tambien podemos usar ${variable} pero el texto entero debe estar entre backticks (`)
+    
+    alert(`Estuve en ${ciudad} y me acordé de ti.`);
+
+    */
 }
